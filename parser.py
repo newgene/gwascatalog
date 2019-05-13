3d389fe7cd76b73eee681510fce80f8ad3c7d313
sookieliao <kevinxin@scripps.edu>
2019-05-13 12:33:42 -0700

    change biothigns_client to requests

--- parser.py
+++ parser.py
@@ -1,7 +1,8 @@
 import os
 import unicodedata
 from collections import defaultdict
-from biothings_client import get_client
+#from biothings_client import get_client
+import requests
 import re
 
 from csv import DictReader
@@ -70,8 +71,12 @@ def batch_query_hgvs_from_rsid(rsid_list):
             batch = rsid_list[i: i+1000]
         else:
             batch = rsid_list[i:]
+        """
         params = ','.join(batch)
         res = variant_client.getvariants(params, fields="_id")
+        """
+        data = {"_id": ','.join(batch), "fields": "_id"}
+        res = requests.post('http://myvariant.info/v1/variant', data=data).json()
         print("currently processing {}th variant".format(i))
         for _doc in res:
             if '_id' not in _doc:
